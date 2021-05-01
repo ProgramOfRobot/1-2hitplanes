8c5ca6e26524882f98ac6c51d2f652df184060c7
Helloworldzz <58582177+ProgramOfRobot@users.noreply.github.com>
2021-05-01 10:32:35 +0800

    Added methods and functions to use

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
+function Init () {
+	
+}
+function GameLoop () {
+	
+}
