b7fe4ebfa7cc62fc5153b9e5ff7dafc385df1866
Helloworldzz <58582177+ProgramOfRobot@users.noreply.github.com>
2021-05-01 21:24:24 +0800

    Not really ok

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (isStart) {
-        Bullet = game.createSprite(OurPlane.get(LedSpriteProperty.X), 3)
+        Bullet = game.createSprite(OurPlane.get(LedSpriteProperty.X), OurPlane.get(LedSpriteProperty.Y))
         Bullet.set(LedSpriteProperty.Brightness, 100)
         while (true) {
             basic.pause(Our_Bullets_Speet)
-            Bullet.change(LedSpriteProperty.Y, -1)
+            Bullet.change(LedSpriteProperty.X, -1)
             if (EnemyPlane.isTouching(Bullet) || Bullet.isTouching(EnemyPlane)) {
                 Bullet.delete()
                 ResetSpritePosition(EnemyPlane)
                 game.addScore(1)
                 break;
             }
-            if (Bullet.get(LedSpriteProperty.Y) == 0) {
+            if (Bullet.get(LedSpriteProperty.X) == 0) {
                 Bullet.delete()
                 break;
             }
@@ -20,16 +20,16 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 input.onButtonPressed(Button.A, function () {
     if (isStart) {
-        OurPlane.change(LedSpriteProperty.X, -1)
+        OurPlane.change(LedSpriteProperty.Y, -1)
     }
 })
 function ResetSpritePosition (sprite: game.LedSprite) {
-    sprite.set(LedSpriteProperty.Y, 0)
-    sprite.set(LedSpriteProperty.X, randint(0, 4))
+    sprite.set(LedSpriteProperty.X, 0)
+    sprite.set(LedSpriteProperty.Y, randint(0, 4))
 }
 input.onButtonPressed(Button.B, function () {
     if (isStart) {
-        OurPlane.change(LedSpriteProperty.X, 1)
+        OurPlane.change(LedSpriteProperty.Y, 1)
     }
 })
 function Init () {
@@ -44,13 +44,13 @@ function GameLoop () {
     game.setLife(5)
     game.setScore(0)
     isStart = 1
-    OurPlane = game.createSprite(2, 4)
-    EnemyPlane = game.createSprite(randint(0, 4), 0)
-    EnemyPlane.set(LedSpriteProperty.Direction, 180)
+    OurPlane = game.createSprite(4, 2)
+    EnemyPlane = game.createSprite(0, randint(0, 4))
+    EnemyPlane.set(LedSpriteProperty.Direction, 90)
     while (game.isRunning()) {
         basic.pause(Enemy_Planes_Speet)
         EnemyPlane.move(1)
-        if (EnemyPlane.get(LedSpriteProperty.Y) == 4 || OurPlane.isTouching(EnemyPlane)) {
+        if (EnemyPlane.get(LedSpriteProperty.X) == 4 || OurPlane.isTouching(EnemyPlane)) {
             game.removeLife(1)
             ResetSpritePosition(EnemyPlane)
         }
